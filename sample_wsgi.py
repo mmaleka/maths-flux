70cb76a53e09649661b11def853e2e18dd94234a
Mpho <mpho.maleka3@gmail.com>
2018-02-04 19:48:39 +0200

    sample wsgi modified

--- sample_wsgi.py
+++ sample_wsgi.py
@@ -5,7 +5,7 @@ path = '/home/pythonanywheremmaleka/blog' # Use PythonAnywhere username here
 if path not in sys.path:
     sys.path.append(path)
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'blog.blog.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'blog.settings'
 
 from django.core.wsgi import get_wsgi_application
 from django.contrib.staticfiles.handlers import StaticFilesHandler
